752b40e01158e3b37318959fb89707cd809989ff
Tharsa Selvam <tharsas@ulrik.uio.no>
2024-04-05 12:32:53 +0200

    feat:SwimspotListFromCSV (#44)
    * feat:SwimspotListFromCSV
    
    Made a csv w/ swimspots, function who returns a list of swimspot objects
    
    * feat:changedFolderCSVRead
    
    Bytta til en swimspotDataSource
    
    * feat:Small fixes on Datasource
    
    Fjerna gamle imports

--- app/src/main/java/no/uio/ifi/in2000/team22/badeapp/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team22/badeapp/MainActivity.kt
@@ -1,27 +1,32 @@
 package no.uio.ifi.in2000.team22.badeapp
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import no.uio.ifi.in2000.team22.badeapp.data.SwimspotDataSource
 import no.uio.ifi.in2000.team22.badeapp.ui.screens.favorites.FavoritesScreen
 import no.uio.ifi.in2000.team22.badeapp.ui.screens.home.HomeScreen
 import no.uio.ifi.in2000.team22.badeapp.ui.screens.search.SearchScreen
-import no.uio.ifi.in2000.team22.badeapp.ui.screens.swimspot.SwimspotScreen
 import no.uio.ifi.in2000.team22.badeapp.ui.theme.BadeappTheme
 
+
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        //installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContent {
             BadeappTheme {
                 Surface(
@@ -37,10 +42,11 @@ class MainActivity : ComponentActivity() {
                         }
                         composable("favorites") { FavoritesScreen(navcontroller) }
                         composable("search") { SearchScreen(navcontroller) }
-                        composable("swimspot") { SwimspotScreen(navcontroller) }
+                        //composable("swimspot") { SwimspotScreen(navcontroller) }
                         //settings?
                     }
-                    //StoppScreen()
+                    val x = SwimspotDataSource(this)
+                    x.lesFraFil()
                 }
             }
         }
