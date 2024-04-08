caa2d7f390aa4bb9c841666bdc15ed3bc87aae1c
Bror Reinskou Birkeland <brorrb@ifi.uio.no>
2024-04-08 13:13:57 +0200

    feat: api call

--- app/src/main/java/no/uio/ifi/in2000/team22/badeapp/data/oceanforecastApi/OceanforecastDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/team22/badeapp/data/oceanforecastApi/OceanforecastDataSource.kt
@@ -1,7 +1,20 @@
 package no.uio.ifi.in2000.team22.badeapp.data.oceanforecastApi
 
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.http.URLProtocol
+import io.ktor.http.path
+import io.ktor.serialization.gson.gson
+import no.uio.ifi.in2000.team22.badeapp.data.MetAPI
+import no.uio.ifi.in2000.team22.badeapp.model.forecast.Weather
 
-data class OceanAPI(
+
+data class OceanForecast(
     val type: String,
     val geometry: Geometry,
     val properties: Properties
@@ -53,6 +66,75 @@ data class Details(
 
 
 class OceanforecastDataSource {
+    private val client =
+        HttpClient {
+            defaultRequest {
+                header(MetAPI.API_KEY_NAME, MetAPI.API_KEY)
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = MetAPI.HOST
+                    path(MetAPI.OceanForecast.PATH, MetAPI.OceanForecast.TYPE)
+                }
+            }
 
+            install(ContentNegotiation) {
+                gson()
+            }
+        }
+
+    private suspend fun fetch(lat: Double, lon: Double): OceanForecast? {
+        return try {
+            val response = client.get {
+                url {
+                    parameters.append("lat", lat.toString())
+                    parameters.append("lon", lon.toString())
+                }
+            }
+            response.body<OceanForecast>()
+        } catch (e: Exception) {
+            Log.e("OceanForecastDataSource", e.message.toString())
+            Log.e("OceanForecastDataSource", e.stackTrace.toString())
+            null
+        }
+    }
+
+
+    suspend fun fetchTemperature(lat: Double, lon: Double): Weather {
+        //Returns a Weather object based in the current time
+        // lat and lon: latitude and longitude
+        // hourOffset: Weather this many hours into the future.
+        if (hourOffset > 54) {
+            Log.w(
+                "LocationforecastDataSource",
+                "fetchWeather() might return null Weather parameters (hourOffset > 54)"
+            )
+        }
+
+        try {
+            val forecast = fetch(lat, lon)
+            val timeseries = forecast?.properties?.timeseries?.get(hourOffset)
+            return Weather(
+                time = timeseries?.time,
+                airTemperature = timeseries?.data?.instant?.details?.air_temperature,
+                symbolCode = timeseries?.data?.next_1_hours?.summary?.symbol_code,
+                windSpeed = timeseries?.data?.instant?.details?.wind_speed,
+                windFromDirection = timeseries?.data?.instant?.details?.wind_from_direction,
+                uvIndex = timeseries?.data?.instant?.details?.ultraviolet_index_clear_sky,
+                precipitationNextHour = timeseries?.data?.next_1_hours?.details?.precipitation_amount
+            )
+        } catch (e: Exception) {
+            Log.e("LocationforecastDataSource", e.message.toString())
+            Log.e("LocationforecastDataSource", e.stackTrace.toString())
+            return Weather(
+                time = null,
+                airTemperature = null,
+                symbolCode = null,
+                windSpeed = null,
+                windFromDirection = null,
+                uvIndex = null,
+                precipitationNextHour = null
+            )
+        }
+    }
 
 }
