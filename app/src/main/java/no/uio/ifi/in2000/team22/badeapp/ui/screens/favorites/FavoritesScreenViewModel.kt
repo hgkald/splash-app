f86025cf83cbc581e1636ed4313fd3c3afacf45b
Tobias Lund Brekkå <tobialb@ifi.uio.no>
2024-04-11 15:47:00 +0200

    refactor: Could not find bug, second try, new files

--- app/src/main/java/no/uio/ifi/in2000/team22/badeapp/ui/screens/favorites/FavoritesScreenViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team22/badeapp/ui/screens/favorites/FavoritesScreenViewModel.kt
@@ -1,17 +1,1 @@
 package no.uio.ifi.in2000.team22.badeapp.ui.screens.favorites
-
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import no.uio.ifi.in2000.team22.badeapp.model.swimspots.Swimspot
-
-data class FavListUiState(
-    val favList: List<Swimspot> = emptyList()
-)
-
-class FavoritesScreenViewModel : ViewModel() {
-    private val _favUiState = MutableStateFlow(FavListUiState())
-    val favUiState : StateFlow<FavListUiState> = _favUiState.asStateFlow()
-
-}
