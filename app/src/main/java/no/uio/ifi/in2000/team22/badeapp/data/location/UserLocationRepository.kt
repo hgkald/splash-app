11e8fc8a88b73872b91405b083daaac58842357b
pederaha <pederaha@ulrik.uio.no>
2024-04-25 15:59:49 +0200

    feat: add support for location updates in location repo

--- app/src/main/java/no/uio/ifi/in2000/team22/badeapp/data/location/UserLocationRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/team22/badeapp/data/location/UserLocationRepository.kt
@@ -1,11 +1,18 @@
 package no.uio.ifi.in2000.team22.badeapp.data.location
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Looper
+import android.util.Log
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,13 +24,44 @@ data class LocationState(
 )
 
 class UserLocationRepository(
-    val context: Context
+    private val context: Context
 ) {
-    val locationClient =
+    private val locationClient =
         LocationServices.getFusedLocationProviderClient(context)
-    var lastKnownLocation: Location? = null;
 
     private var _locationState = MutableStateFlow(LocationState())
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            val newLocation = p0.lastLocation
+            val oldLocation = _locationState.value.lastKnownLocation
+
+            val updateLocation = {
+                Log.i("UserLocationRepo", "Updating current location")
+                _locationState.update {
+                    it.copy(
+                        lastKnownLocation = p0.lastLocation
+                    )
+                }
+            }
+
+            if (oldLocation != null && newLocation == null) {
+                return
+            }
+
+            if (oldLocation == null) {
+                updateLocation()
+            }
+
+            if (oldLocation != null &&
+                newLocation != null &&
+                oldLocation.distanceTo(newLocation) > 200
+            ) {
+                updateLocation()
+            }
+
+        }
+    }
 
     fun observe(): StateFlow<LocationState> = _locationState.asStateFlow()
 
@@ -49,7 +87,29 @@ class UserLocationRepository(
             )
         }
 
+        Log.i("UserLocationRepo", "Current permission state is ${fineLocation || coarseLocation}")
+
         return (fineLocation || coarseLocation)
+    }
+
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates() {
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_LOW_POWER,
+            10_000
+        ).build()
+
+        if (checkPermissions() == true) {
+            locationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+    fun stopLocationUpdates() {
+        locationClient.removeLocationUpdates(locationCallback)
     }
 
     init {
